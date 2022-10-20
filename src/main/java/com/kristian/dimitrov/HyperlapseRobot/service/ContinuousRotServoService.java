5815869b3325bb9a064d8d0b089df8ed211e8076
krisisurf <krisisurf@gmail.com>
2022-10-20 20:46:19 +0300

    Service for controlling Continuous Rotation Servo

---
+++ src/main/java/com/kristian/dimitrov/HyperlapseRobot/service/ContinuousRotServoService.java
@@ -0,0 +1,7 @@
+package com.kristian.dimitrov.HyperlapseRobot.service;
+
+public interface ContinuousRotServoService {
+    void stop();
+    void rotateClockwise();
+    void rotateCounterClockwise();
+}
