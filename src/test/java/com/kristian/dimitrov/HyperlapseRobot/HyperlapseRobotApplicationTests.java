dab8844d9c2c20c22a0d830b9f00faabec3d405d
krisisurf <61236255+krisisurf@users.noreply.github.com>
2022-10-20 19:07:46 +0300

    Uploading Java Spring project structure

---
+++ src/test/java/com/kristian/dimitrov/HyperlapseRobot/HyperlapseRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristian.dimitrov.HyperlapseRobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperlapseRobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
