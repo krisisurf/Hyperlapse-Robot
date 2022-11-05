0d1b0e168521b3e2a416e6284a5ce8cd0a15e764
krisisurf <krisisurf@gmail.com>
2022-11-05 13:04:46 +0300

    I2C for sending string

---
+++ java/Hyperlapse-Robot/src/main/java/com/kristian/dimitrov/HyperlapseRobot/service/ArduinoService.java
@@ -0,0 +1,10 @@
+package com.kristian.dimitrov.HyperlapseRobot.service;
+
+public interface ArduinoService {
+
+    /**
+     * Sends json formatted string with rules for the arduino over I2C protocol.
+     * @param json json string with rules.
+     */
+    void sendJsonString(String json);
+}
---
+++ java/Hyperlapse-Robot/src/main/java/com/kristian/dimitrov/HyperlapseRobot/service/impl/ArduinoServiceImpl.java
@@ -0,0 +1,36 @@
+package com.kristian.dimitrov.HyperlapseRobot.service.impl;
+
+import com.kristian.dimitrov.HyperlapseRobot.service.ArduinoService;
+import com.kristian.dimitrov.HyperlapseRobot.utils.Logger;
+import org.springframework.stereotype.Service;
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+import com.pi4j.io.i2c.I2CFactory;
+import java.io.IOException;
+import java.util.Arrays;
+
+@Service
+public class ArduinoServiceImpl implements ArduinoService {
+
+    private final int DEVICE_ADDRESS = 0x04;
+    private final int BUS_NUMBER = I2CBus.BUS_1;
+
+    @Override
+    public void sendJsonString(String json) {
+        try {
+            Logger.makeLog("Creating I2C bus", new Throwable());
+            I2CBus bus = I2CFactory.getInstance(BUS_NUMBER);
+
+            Logger.makeLog("Creating I2C device", new Throwable());
+            I2CDevice device = bus.getDevice(DEVICE_ADDRESS);
+
+            byte[] writeData = json.getBytes();
+
+           Logger.makeLog("Writing bytes: " + writeData, new Throwable());
+           device.write(writeData, 0, writeData.length);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
