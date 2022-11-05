6cfb4b84a26a7d819121e1f40778bd4415e60c7c
krisisurf <krisisurf@gmail.com>
2022-11-05 11:47:13 +0300

    deletion of servo motor files

--- java/Hyperlapse-Robot/src/main/java/com/kristian/dimitrov/HyperlapseRobot/exceptions/FrequencyOutOfBoundsException.java
+++
@@ -1,8 +0,0 @@
-package com.kristian.dimitrov.HyperlapseRobot.exceptions;
-
-public class FrequencyOutOfBoundsException extends RuntimeException{
-
-    public FrequencyOutOfBoundsException(int target, int min, int max){
-        super("Target frequency ( " + target + "Hz ) out of bounds [ " + min + " : " + max + " ]");
-    }
-}
