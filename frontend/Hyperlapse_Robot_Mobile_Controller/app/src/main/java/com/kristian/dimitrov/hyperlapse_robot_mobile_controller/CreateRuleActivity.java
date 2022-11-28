d22c67cd6aaca4d9e42b5e51147c2063acb47a66
Kristian <krisisurf@gmail.com>
2022-11-28 12:48:44 +0200

    main and create rule activities core

--- frontend/Hyperlapse_Robot_Mobile_Controller/app/src/main/java/com/kristian/dimitrov/hyperlapse_robot_mobile_controller/CreateRuleActivity.java
+++ frontend/Hyperlapse_Robot_Mobile_Controller/app/src/main/java/com/kristian/dimitrov/hyperlapse_robot_mobile_controller/CreateRuleActivity.java
@@ -1,14 +1,25 @@
 package com.kristian.dimitrov.hyperlapse_robot_mobile_controller;
 
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 import android.view.View;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
 
 public class CreateRuleActivity extends AppCompatActivity {
 
-    public void buttonCancle(View view) {
-        startActivity(new Intent(CreateRuleActivity.this, MainActivity.class));
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_rule);
+        setTitle("Create a new rule");
+
+        Button buttonCancle = findViewById(R.id.btn_cancle);
+        buttonCancle.setOnClickListener(this::cancleButton);
+    }
+
+    private void cancleButton(View view) {
+        finish();
     }
 }
--- frontend/Hyperlapse_Robot_Mobile_Controller/app/src/main/java/com/kristian/dimitrov/hyperlapse_robot_mobile_controller/MainActivity.java
+++ frontend/Hyperlapse_Robot_Mobile_Controller/app/src/main/java/com/kristian/dimitrov/hyperlapse_robot_mobile_controller/MainActivity.java
@@ -1,10 +1,16 @@
 package com.kristian.dimitrov.hyperlapse_robot_mobile_controller;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Hyperlapse Robot Controller");
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton buttonSendRules = findViewById(R.id.btn_send_rules);
+        //fab.setOnClickListener(this::openCreateRuleActivity);
+
+        Button buttonAddRule = findViewById(R.id.btn_add_rule);
+        buttonAddRule.setOnClickListener(this::openCreateRuleActivity);
     }
 
-    public void buttonAddRule(View view) {
-        setContentView(R.layout.rule_creator);
+    private void openCreateRuleActivity(View view){
+        Intent intent = new Intent(MainActivity.this, CreateRuleActivity.class);
+        startActivity(intent);
     }
 }
