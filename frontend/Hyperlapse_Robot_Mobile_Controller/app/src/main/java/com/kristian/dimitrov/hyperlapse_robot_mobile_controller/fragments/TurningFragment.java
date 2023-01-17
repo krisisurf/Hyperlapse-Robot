9124fcc9048295d205aa9df63119e9f4b97d78ec
Kristian Dimitrov <krisisurf@gmail.com>
2023-01-17 11:18:26 +0200

    turning layout design

--- frontend/Hyperlapse_Robot_Mobile_Controller/app/src/main/java/com/kristian/dimitrov/hyperlapse_robot_mobile_controller/fragments/TurningFragment.java
+++ frontend/Hyperlapse_Robot_Mobile_Controller/app/src/main/java/com/kristian/dimitrov/hyperlapse_robot_mobile_controller/fragments/TurningFragment.java
@@ -10,6 +10,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.kristian.dimitrov.hyperlapse_robot_mobile_controller.R;
+import com.kristian.dimitrov.hyperlapse_robot_mobile_controller.activities.NumberInputPopupDialog;
+import com.kristian.dimitrov.hyperlapse_robot_mobile_controller.entity.RuleEntity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,13 +19,43 @@ import com.kristian.dimitrov.hyperlapse_robot_mobile_controller.R;
  */
 public class TurningFragment extends Fragment {
 
+    private static final String TAG = "TurningFragment";
+
+    public static final String NUMBER_INPUT_POPUP_DIALOG_PARAM = "nipd";
+    public static final String RULE_ENTITY_PARAM = "re";
+
+    private NumberInputPopupDialog numberInputPopupDialog;
+    private RuleEntity ruleEntity;
+
     public TurningFragment() {
         // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment TurningFragment.
+     */
+    public static TurningFragment newInstance(NumberInputPopupDialog numberInputPopupDialog, RuleEntity ruleEntity) {
+        TurningFragment fragment = new TurningFragment();
+
+        Bundle args = new Bundle();
+        args.putSerializable(NUMBER_INPUT_POPUP_DIALOG_PARAM, numberInputPopupDialog);
+        args.putSerializable(RULE_ENTITY_PARAM, ruleEntity);
+
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            numberInputPopupDialog = (NumberInputPopupDialog) getArguments().getSerializable(NUMBER_INPUT_POPUP_DIALOG_PARAM);
+            ruleEntity = (RuleEntity) getArguments().getSerializable(RULE_ENTITY_PARAM);
+        }
     }
 
     @Override
@@ -36,5 +68,7 @@ public class TurningFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
